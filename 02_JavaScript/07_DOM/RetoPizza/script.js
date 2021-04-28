9f7446e07897c4902049a83f44048ad36b19d515
Pedro A. González <pgonzalez@post.com>
2021-04-28 00:01:36 -0500

    Agregar reto pizza

---
+++ 02_JavaScript/07_DOM/RetoPizza/script.js
@@ -0,0 +1,13 @@
+console.log('Hola desde script.js del Reto Pizza 🍕');
+
+//-------------------> Challenge <------------------
+// * Usar Bootstrap *
+// Preguntar Nombre
+// Preguntar Dirección
+// Preguntar Teléfono
+// Preguntar si quieren pizza con piña o sin
+// respuestas: sí o no
+// -> mostrar las respuestas en una card de Bootstrap después de presionar un botón
+
+// PASOS SUGERIDOS
+// Paso 1: Declarar los elementos (inputs y respuestas) que vamos a usar del html (getElementById('id') es una forma)
