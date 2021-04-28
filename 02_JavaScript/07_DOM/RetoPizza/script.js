56e0e373a4df26e70d41cc1bfe8d980fd6ddf863
Pedro A. González <pgonzalez@post.com>
2021-04-28 02:12:29 -0500

    Agregar recomendaciones al reto pizza

--- 02_JavaScript/07_DOM/RetoPizza/script.js
+++ 02_JavaScript/07_DOM/RetoPizza/script.js
@@ -10,4 +10,11 @@ console.log('Hola desde script.js del Reto Pizza 🍕');
 // -> mostrar las respuestas en una card de Bootstrap después de presionar un botón
 
 // PASOS SUGERIDOS
-// Paso 1: Declarar los elementos (inputs y respuestas) que vamos a usar del html (getElementById('id') es una forma)
+// 1: Declarar los elementos que vamos a usar del html (getElementById('id') es una forma)
+// 2: Declarar la función que generará la card con las respuestas
+// 3: Agregar el event listener al botón de ordenar para que cuando se le haga click ejecute la función declarada en el paso 2
+
+// PUNTOS EXTRA!! :)
+// * Condicionar que todos los inputs deban ser llenados para que se despliegue la card
+// * Cambiar la imagen a la card una para la pizza con piña y otra para la pizza sin piña
+// * Evitar que la respuesta de la piña se escriba en mayúscula o cambiar por checkbox o radio button
