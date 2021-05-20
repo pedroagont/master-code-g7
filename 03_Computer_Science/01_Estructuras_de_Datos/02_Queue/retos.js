11ecd403bf199a19e730202c49ed885ba0ef05d9
Pedro A. González <pgonzalez@post.com>
2021-05-19 21:16:05 -0500

    Agregar retos clase colas

---
+++ 03_Computer_Science/01_Estructuras_de_Datos/02_Queue/retos.js
@@ -0,0 +1,26 @@
+console.log('Hola desde retos! 👋');
+
+// 1.- Se tiene una cola de colores y se tiene que dividir en dos colas, respetando el orden y alternando a partir de su índice. los pares en una y los nones en otra
+// Cola original: [ amarillo, rosa, rojo, verde, azul, negro, morado, blanco]
+// Cola 1: [ amarillo, rojo, azul, morado]
+// Cola 2: [rosa, verde, negro, blanco]
+
+// 2.- Se tiene una cola en la cual se han repartido tickets con el orden de atención.
+// Sin embargo, llegada la hora de inicio hay muchos “colados”, es por esto que se le ordena al vigilante que retire a todos aquellos que no tienen ticket.
+// Muestra la cola inicial, qué elementos fueron retirados de la cola y la cola final.
+//
+// Sugerencia: desencolar cada elemento, si tiene el ticket se vuelve a encolar, si no se retira.
+//
+// Cola: [
+// { user:‘User1’, ticket:true },
+// { user:‘User2’, ticket:true },
+// { user:‘User3’, ticket:false },
+// { user:‘User4’, ticket:true },
+// { user:‘User5’, ticket:false },
+// { user:‘User6’, ticket:false },
+// { user:‘User7’, ticket:true },
+// { user:‘User8’, ticket:true },
+// { user:‘User9’, ticket:true },
+// { user:‘User10’, ticket:false },
+// { user:‘User11’, ticket:true },
+// ];
