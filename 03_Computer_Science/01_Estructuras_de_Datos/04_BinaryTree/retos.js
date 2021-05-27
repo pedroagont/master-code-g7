efb7f2dbb70a041b31667677fc2d2abf6e6ec01f
Pedro A. González <pgonzalez@post.com>
2021-05-27 17:02:11 -0500

    Agregar retos binary tree

---
+++ 03_Computer_Science/01_Estructuras_de_Datos/04_BinaryTree/retos.js
@@ -0,0 +1,9 @@
+console.log('Hola desde retos! 👋');
+
+// 1. Escribe una función que dados dos árboles binarios A y B, determine si son idénticos o no.
+
+// 2. Escribe una función que dado un árbol binario A, obtenga una copia B del mismo.
+
+// 3. Escribe una función que visualice los nodos que están en el nivel n de un árbol binario.
+
+// 4. Escribe una función que devuelva el número de hojas de un árbol binario.
