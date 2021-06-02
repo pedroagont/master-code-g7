dfcc28201368bbce05ebc467c2d55a8c1a3ce12a
Pedro A. González <pgonzalez@post.com>
2021-06-01 21:02:36 -0500

    Agregar bubble sort y retos

---
+++ 03_Computer_Science/03_Algoritmos_de_Ordenamiento/01_Bubble_Sort/retos.js
@@ -0,0 +1,21 @@
+console.log('Hola desde retos! 👋');
+
+// 1.- Crea una función que ordene de forma descendente  un arreglo de números
+//     Ej. Entrada --> [9,3,1,6,5,88,-1,2,7]
+//     Salida → [88,9,7,6,5,3,1,-1]
+
+// 2.- Crear una función que ordene una lista de caracteres según su valor decimal de acuerdo a la tabla ascii ref: http://www.asciitable.com/
+//     Ej. Entrada --> [‘b’,‘h’,‘w’,‘e’,‘a’]
+//     Salida --> [‘a’,‘b’,‘e’,‘h’,‘w’]
+
+// 3.- Crear una clase Alumno con los siguientes datos:
+//     - Nombre y calificaciones(arreglo de 5 números) y tiene un método para obtener el promedio
+//     - Crear otra clase llamada Salon que tenga un método que ordene alumnos de mayor a menor promedio
+
+// 4.- Crear una funcion que ordene palabras de menor numero de letras a mayor
+//     Ej. Entrada --> [“adios”,“hola”,“maximo”,“uno”,“despedida”]
+//     Salida --> [“”uno,“hola”,“adios”,“maximo”,“despedida”]
+
+// 5.- Crear una función que ordene un arreglo de coordenadas x, y desde a partir del punto mas cercano a 0,0
+//     - Ej. Entrada --> [[7,3],[2,2],[1,0],[4,3]]
+//     - Ej. Salida --> [[1,0],[2,2],[4,3],[7,3]]
