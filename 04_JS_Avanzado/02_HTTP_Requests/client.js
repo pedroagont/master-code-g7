30addacb351437766c77cf0455045df6e321c4b0
Pedro A. González <pgonzalez@post.com>
2021-06-29 19:23:12 -0500

    Reestructurar clase Node+Peticiones

--- 04_JS_Avanzado/02_HTTP_Requests/client.js
+++
@@ -1,8 +0,0 @@
-const request = require("request");
-
-request.get('https://pokeapi.co/api/v2/pokemon/ditto', (err, res, body) => {
-    console.log(res.statusCode);
-    const pokemon = JSON.parse(body);
-    console.log(pokemon);
-    console.log(`Mi pokemon es ${pokemon.name}`);
-});
