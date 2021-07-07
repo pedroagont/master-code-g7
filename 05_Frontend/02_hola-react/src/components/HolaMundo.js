9b7d7ede9518a82c26cc65ce4ad569dfa989f4a2
Pedro A. González <pgonzalez@post.com>
2021-07-07 12:00:34 -0500

    Agregar ejemplo de estilos y clases, agregar comentarios de referencia

--- 05_Frontend/02_hola-react/src/components/HolaMundo.js
+++ 05_Frontend/02_hola-react/src/components/HolaMundo.js
@@ -1,8 +1,12 @@
+// COMPONENTES
+// Un componente es una pieza de código que podemos manipular y reutilizar a nuestra conveniencia en ReactJS
+// En otros términos, podemos describir un componente como un objeto/función que renderiza/retorna el código que necesitamos incrustar en nuestra app
+
 function HolaMundo() {
   return (
     <h2>Hola desde el componente HolaMundo! 👋🌎</h2>
   );
 }
 
-
+// Para permitir que otros componetes tengan acceso a esta pieza de código, necesitamos pedirle a JS que lo exporte
 export default HolaMundo;
--- 05_Frontend/02_hola-react/src/index.js
+++ 05_Frontend/02_hola-react/src/index.js
@@ -1,3 +1,7 @@
+// INDEX.JS
+// El archivo index.js es el que importa las librerías de React
+// También es el archivo que importa nuestro componente principal App.js y lo renderiza en el nodo id="root" de nuestro archivo HTML
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
