286d8325f9a33b81df2959d8b11f0807285bf67b
Pedro A. González <pgonzalez@post.com>
2021-07-06 21:31:12 -0500

    Clase intro a create-react-app, crear componente HolaMundo

---
+++ 05_Frontend/02_hola-react/src/App.js
@@ -0,0 +1,13 @@
+import HolaMundo from './components/HolaMundo.js';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hola desde App!📲</h1>
+      <HolaMundo />
+    </>
+  );
+}
+
+export default App;
