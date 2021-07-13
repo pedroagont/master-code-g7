7c54d207c46faeb6e7cfb36c038e3b48050a2db8
Pedro A. González <pgonzalez@post.com>
2021-07-13 02:33:20 -0500

    Agregar comentarios + links de referencia + estilos

--- 05_Frontend/03_class-components/src/App.js
+++ 05_Frontend/03_class-components/src/App.js
@@ -1,14 +1,15 @@
-import ComponenteClase from './components/ComponenteClase'
-import Tarjeta from './components/Tarjeta/'
+import ComponenteClase from './components/ComponenteClase';
+import Tarjeta from './components/Tarjeta/';
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Hola desde App! 👋</h1>
       <ComponenteClase />
       <Tarjeta nombre="Pedro" direccion="Cancún" telefono="9988458554" correo="cancun@devf.mx" />
-      <Tarjeta nombre="Sergio" direccion="Querétaro" telefono="444666888" correo="sergio@devf.mx" />
-    </div>
+      <Tarjeta nombre="Sergio" direccion="Querétaro" telefono="444666888" correo="sergio@devf.mx" color="royalblue"/>
+      <Tarjeta nombre="Laura" direccion="Colombia" telefono="123123123" correo="laura@cool.com" color="tomato"/>
+    </>
   );
 }
 
