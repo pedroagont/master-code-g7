36dab067c5f4d545ff6851e35cc38a8885dcdeeb
Pedro A. González <pgonzalez@post.com>
2021-07-21 18:31:03 -0500

    Detalles clase react lifecycle

--- 05_Frontend/04_react_lifecycle/src/components/Personajes.js
+++ 05_Frontend/04_react_lifecycle/src/components/Personajes.js
@@ -3,24 +3,38 @@ import Card from './Card';
 import axios from 'axios';
 
 class Personajes extends React.Component {
+
+  // 1. INICIALIZANDO EL COMPONENTE
+
+  // -----> Método constructor()
+  // El método constructor inicia nuestro componente y se debe llamar antes de cualquier otra cosa
+  // Se inicia con la función super para heredar las propiedades y métodos de componente React
+  // Tanto el constructor como la funcion super se pasan con los props
+
   constructor(props) {
     super(props);
+
+    // -----> Variables de estado this.state
+    // Una vez inicializado el componente, podemos acceder a sus variables de estado, es decir, al objeto state de los componentes React
+    // El state es una propiedad en la que podemos almacenar valores que forman parte del componente
+    // Cuando un valor del estado cambia, el componente se vuelve a renderizar
+    // Link de referencia: https://www.w3schools.com/react/react_state.asp
+
+    // Para acceder al estado podemos acceder a la instancia this + el objeto state
     this.state = {
       personajes: [],
     }
+
   }
 
+  // 2. PARA MONTAR EL COMPONENTE
   componentDidMount() {
     const URL_API_RICKMORTY = 'https://rickandmortyapi.com/api/character';
-
     axios.get(URL_API_RICKMORTY)
       .then(response => {
-        const personajes = response.data.results
-        this.setState({ personajes });
+        this.setState({ personajes: response.data.results });
       })
-      .catch(error => {
-        console.log(error);
-      })
+      .catch(error => console.log(error))
   }
 
   renderizarPersonajes() {
@@ -28,16 +42,18 @@ class Personajes extends React.Component {
     if (personajes.length === 0) {
       return <h2>Cargando personajes...</h2>
     } else if (personajes.length > 0){
-      return <>
-              <h2> Se encontraron { personajes.length } personajes </h2>
-              <div>
-                {
-                  personajes.map(personaje => {
-                    return <Card key={ personaje.id } nombre={personaje.name} especie={personaje.species} origen={personaje.origin.name} imagen={personaje.image}/>
-                  })
-                }
-              </div>
-            </>
+      return (
+        <>
+          <h2> Se encontraron { personajes.length } personajes </h2>
+          <div>
+            {
+              personajes.map(personaje => {
+                return <Card key={ personaje.id } nombre={personaje.name} especie={personaje.species} origen={personaje.origin.name} imagen={personaje.image}/>
+              })
+            }
+          </div>
+        </>
+      );
     }
   }
 
