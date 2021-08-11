186a82670b4ceb3a2501b6e29e065dd0d3fe9b3d
Pedro A. González <pgonzalez@post.com>
2021-08-11 00:12:02 -0500

    Corregir descripción, cambiar propiedad style por clase list-unstyled

--- 05_Frontend/07_react-bootstrap/src/components/Footer.js
+++ 05_Frontend/07_react-bootstrap/src/components/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
       <Container fluid className="bg-primary text-light p-5">
         <Row>
           <Col>
-            <p>Soy la descripción de esta súper aplicación increíble que tenemos hecha en ReactJS utilizando la librería de Bootstrap! Me hace muy feliz poder aprender estas herramientas</p>
+            <p>Soy la descripción de esta súper aplicación increíble que tenemos hecha en ReactJS utilizando la librería de Bootstrap!</p>
           </Col>
           <Col>
-            <ul style={{ listStyleType: 'none' }}>
+            <ul className="list-unstyled">
               <li><a href="/" className="text-light">Quiénes Somos</a></li>
               <li><a href="/" className="text-light">Bolsa de trabajo</a></li>
               <li><a href="/" className="text-light">Contacto</a></li>
             </ul>
           </Col>
           <Col>
-            <ul style={{ listStyleType: 'none' }}>
+            <ul className="list-unstyled">
               <li><a href="/" className="text-light">Facebook</a></li>
               <li><a href="/" className="text-light">LinkedIn</a></li>
               <li><a href="/" className="text-light">Twitter</a></li>
