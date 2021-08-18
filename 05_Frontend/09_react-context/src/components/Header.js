9d1da364c347c4cafda2571c06ec38b1e8a2ea39
Pedro A. González <pgonzalez@post.com>
2021-08-18 00:19:16 -0500

    Agregar detalles de estilo

--- 05_Frontend/09_react-context/src/components/Header.js
+++ 05_Frontend/09_react-context/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className={ `header ${toggle ? 'dark' : 'light' }` }>
       <h2>Soy el header! 🎩</h2>
-      <button className={ `boton ${toggle ? 'dark' : 'light' }` } onClick={ toggleFunction }>Cambiar tema: { toggle ? 'Dark' : 'Light' }</button>
+      <button className={ `boton ${toggle ? 'dark' : 'light' }` } onClick={ toggleFunction }>Cambiar tema: { toggle ? '🌚' : '🌞' }</button>
     </div>
   );
 }
