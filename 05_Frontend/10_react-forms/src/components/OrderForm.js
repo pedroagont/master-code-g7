c694b2465e5f451e19698687f014c15a373fd29d
Pedro A. González <pgonzalez@post.com>
2021-08-18 22:58:54 -0500

    Crear hook personalizado useForm y refactorizar OrderForm con callback

--- 05_Frontend/10_react-forms/src/components/OrderForm.js
+++ 05_Frontend/10_react-forms/src/components/OrderForm.js
@@ -1,35 +1,35 @@
+import useForm from '../hooks/useForm';
+
 function OrderForm() {
 
-  const handleChange = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
+  const handleOrderForm = () => {
+    // console.log('Hola desde el callback!', formData);
+    const { pedido, nombre, direccion, telefono } = formData;
+    alert(`Gracias por tu compra ${nombre}! Tu pedido de ${pedido} está en camino a ${direccion} y te avisarán al ${telefono}`);
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form enviado!');
-  }
+  const { formData, handleChange, handleSubmit } = useForm(handleOrderForm);
 
   return (
     <form className="form" onSubmit={ handleSubmit }>
       <h2 className="form-title">Pide tu cupcake aquí! 🧁</h2>
 
-      <label for="pet-select" className="form-label">Sabor de tus cupcakes</label>
-      <select name="pedido" id="pet-select" className="form-input">
-          <option value="" disabled selected>Escoge una opción..</option>
+      <label htmlFor="pet-select" className="form-label">Sabor de tus cupcakes</label>
+      <select name="pedido" id="pet-select" className="form-input" onChange={ handleChange } value={formData.pedido || ''} required>
+          <option value="">Escoge una opción..</option>
           <option value="chocolate">Chocolate</option>
           <option value="vainilla">Vainilla</option>
           <option value="redVelvet">Red Velvet</option>
       </select>
 
       <label className="form-label">Nombre</label>
-      <input className="form-input" type="text" name="nombre" placeholder="Juan Pérez" onChange={ handleChange } autoComplete="off" />
+      <input className="form-input" type="text" name="nombre" placeholder="Juan Pérez" onChange={ handleChange } autoComplete="off" value={formData.nombre || ''} required />
 
       <label className="form-label">Direccion</label>
-      <input className="form-input" type="text" name="direccion" placeholder="Av Litman #34" onChange={ handleChange } autoComplete="off" />
+      <input className="form-input" type="text" name="direccion" placeholder="Av Litman #34" onChange={ handleChange } autoComplete="off" value={formData.direccion || ''} required />
 
       <label className="form-label">Teléfono</label>
-      <input className="form-input" type="text" name="telefono" placeholder="984 362 4557" onChange={ handleChange } autoComplete="off" />
+      <input className="form-input" type="text" name="telefono" placeholder="984 362 4557" onChange={ handleChange } autoComplete="off" value={formData.telefono || ''} required />
 
       <input className="form-submit" type="submit" value="Enviar form!"/>
     </form>
