cec18883aea62fe52888555ff7b87a724ec70d5f
Pedro A. González <pgonzalez@post.com>
2021-09-15 19:10:34 -0500

    Limpieza de componente App en clase react forms

--- 05_Frontend/10_react-forms/src/App.js
+++ 05_Frontend/10_react-forms/src/App.js
@@ -1,14 +1,9 @@
 import Home from './pages/Home';
-// import OrderService from './pages/OrderService';
 
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
