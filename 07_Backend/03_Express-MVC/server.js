573dde68d30624103e5ccade81e07c2390b0ab33
Pedro A. González <pgonzalez@post.com>
2021-09-30 22:20:30 -0500

    Instalar y agregar middleware morgan para registrar accesos al servidor

--- 07_Backend/03_Express-MVC/server.js
+++ 07_Backend/03_Express-MVC/server.js
@@ -1,5 +1,6 @@
 // MODULES
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const routes = require('./routes');
@@ -7,6 +8,7 @@ const routes = require('./routes');
 // MIDDLEWARES
 app.use(express.json()); // para procesar json
 app.use(express.urlencoded({ extended: true })); // para procesar documentos, archivos, imagenes, video, etc
+app.use(morgan('dev')); // para que nos muestre un registro de cada acceso a nuestro servidor
 
 // ENDPOINTS
 app.use('/api/v1', routes);
