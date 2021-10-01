1973c7592e04863fd486ec583b6ed18e30a9cb01
Pedro A. González <pgonzalez@post.com>
2021-09-30 22:37:50 -0500

    Instalar y agregar middleware helmet para mejorar seguridad del servidor

--- 07_Backend/03_Express-MVC/server.js
+++ 07_Backend/03_Express-MVC/server.js
@@ -1,6 +1,7 @@
 // MODULES
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const routes = require('./routes');
@@ -9,6 +10,7 @@ const routes = require('./routes');
 app.use(express.json()); // para procesar json
 app.use(express.urlencoded({ extended: true })); // para procesar documentos, archivos, imagenes, video, etc
 app.use(morgan('dev')); // para que nos muestre un registro de cada acceso a nuestro servidor
+app.use(helmet()); // para aumentar la seguridad de nuestro servidor
 
 // ENDPOINTS
 app.use('/api/v1', routes);
