677d0bab626927a84cb9b13063b68d51b7a850b5
Pedro A. González <pgonzalez@post.com>
2021-10-15 02:48:16 -0500

    Simular acciones del controlador

--- 07_Backend/03_Express-MVC/controller/UsersController.js
+++ 07_Backend/03_Express-MVC/controller/UsersController.js
@@ -6,43 +6,56 @@ const createUser = (req, res) => {
     return res.status(400).send({ message: 'Ingresar email y password' });
   }
 
-  const user = {
+  const newUser = {
     id: Math.floor(Math.random() * 100),
     ...req.body
   };
 
-  res.status(201).send({ message: `Usuario creado con el id: ${user.id}!` });
+  return res.status(201).send({ message: 'Usuario creado!', newUser });
 };
 
 const getAllUsers = (req, res) => {
   // Lógica de read all
-  res.status(200).send({ message: 'Estos son tus usuarios!' });
+  const users = [
+    { id: 1, email: 'pedrito@correo.com', password: 'abc123' },
+    { id: 2, email: 'juanito@correo.com', password: 'qwerty' }
+  ];
+  return res.status(200).send({ message: 'Estos son tus usuarios!', users });
 };
 
 const getUserById = (req, res) => {
   // Lógica de read one
   const { id } = req.params;
-  res.status(200).send({ message: `Este es tu usuario con el id: ${id}!` });
+  const user = { id };
+  return res
+    .status(200)
+    .send({ message: `Este es tu usuario con el id: ${id}!`, user });
 };
 
 const updateUser = (req, res) => {
   // Lógica de update
   const { id } = req.params;
-  res.status(200).send({ message: `Actualizado el usuario con el id: ${id}!` });
+  const updatedUser = { id };
+  return res
+    .status(200)
+    .send({ message: `Actualizado el usuario con el id: ${id}!`, updatedUser });
 };
 
 const updatePartialUser = (req, res) => {
   // Lógica de partial update
   const { id } = req.params;
-  res
-    .status(200)
-    .send({ message: `Actualizado parcialmente el usuario con el id: ${id}!` });
+  const updatedUser = { id };
+  return res.status(200).send({
+    message: `Propiedad actualizada del usuario con el id: ${id}!`,
+    updatedUser
+  });
 };
 
 const deleteUser = (req, res) => {
   // Lógica de delete
   const { id } = req.params;
-  res.status(204).send();
+  delete id;
+  return res.status(204).send();
 };
 
 module.exports = {
