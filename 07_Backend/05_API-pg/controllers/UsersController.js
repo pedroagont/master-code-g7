2a7128aed6054299c42c8d05bbc96b88fc4751e9
Pedro A. González <pgonzalez@post.com>
2021-10-20 21:08:51 -0500

    Refactorizar controladores con models

--- 07_Backend/05_API-pg/controllers/UsersController.js
+++ 07_Backend/05_API-pg/controllers/UsersController.js
@@ -1,59 +1,37 @@
 const UsersModel = require('../models/UsersModel');
 
-const { Pool } = require('pg');
-
-const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'qwe123',
-  port: 5432
-});
-
 const createUser = async (req, res) => {
-  // Lógica de create
+  // TRABAJO DEL CONTROLADOR
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).send({ message: 'Ingresar email y password' });
   }
 
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  const user = await UsersModel.createUser(email, password, db);
+  // INTERACCIÓN CON MODELO
+  const user = await UsersModel.createUser(email, password);
 
+  // RESPUESTA A LA VISTA
   return res.status(201).send({ message: 'Usuario creado!', user });
 };
 
-const getAllUsers = (req, res) => {
-  // Lógica de read all
-
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  db.query('SELECT * FROM users')
-    .then(result => {
-      return res
-        .status(200)
-        .send({ message: 'Estos son tus usuarios!', users: result.rows });
-    })
-    .catch(err => console.error(err.stack));
+const getAllUsers = async (req, res) => {
+  const users = await UsersModel.getAllUsers();
+  return res.status(200).send({ message: 'Estos son tus usuarios!', users });
 };
 
-const getUserById = (req, res) => {
-  // Lógica de read one
+const getUserById = async (req, res) => {
   const { id } = req.params;
 
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  db.query('SELECT * FROM users WHERE user_id = $1', [id])
-    .then(result => {
-      return res.status(200).send({
-        message: `Este es tu usuario con el id: ${id}!`,
-        user: result.rows[0]
-      });
-    })
-    .catch(err => console.error(err.stack));
+  const user = await UsersModel.getUserById(id);
+
+  return res.status(200).send({
+    message: `Este es tu usuario con el id: ${id}!`,
+    user
+  });
 };
 
-const updateUser = (req, res) => {
-  // Lógica de update
+const updateUser = async (req, res) => {
   const { id } = req.params;
   const { email, password } = req.body;
 
@@ -61,22 +39,15 @@ const updateUser = (req, res) => {
     return res.status(400).send({ message: 'Ingresar email y password' });
   }
 
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  db.query(
-    'UPDATE users SET email = $1, password = $2 WHERE user_id = $3 RETURNING *',
-    [email, password, id]
-  )
-    .then(result => {
-      return res.status(200).send({
-        message: `Actualizado el usuario con el id: ${id}!`,
-        user: result.rows[0]
-      });
-    })
-    .catch(err => console.error(err.stack));
+  const user = await UsersModel.updateUser(email, password, id);
+
+  return res.status(200).send({
+    message: `Actualizado el usuario con el id: ${id}!`,
+    user
+  });
 };
 
-const updatePartialUser = (req, res) => {
-  // Lógica de partial update
+const updatePartialUser = async (req, res) => {
   const { id } = req.params;
   const { property, value } = req.body;
 
@@ -84,29 +55,20 @@ const updatePartialUser = (req, res) => {
     return res.status(400).send({ message: 'Ingresar property y value' });
   }
 
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  db.query(
-    `UPDATE users SET ${property} = '${value}' WHERE user_id = ${id} RETURNING *`
-  )
-    .then(result => {
-      return res.status(200).send({
-        message: `Parcialmente actualizado el usuario con el id: ${id}!`,
-        user: result.rows[0]
-      });
-    })
-    .catch(err => console.error(err.stack));
+  const user = await UsersModel.updatePartialUser(property, value, id);
+
+  return res.status(200).send({
+    message: `Parcialmente actualizado el usuario con el id: ${id}!`,
+    user
+  });
 };
 
-const deleteUser = (req, res) => {
-  // Lógica de delete
+const deleteUser = async (req, res) => {
   const { id } = req.params;
 
-  // A PARTIR DE AQUÍ VA LA CAPA QUE ACCEDE A LA BASE DE DATOS (MODELS)
-  db.query('DELETE FROM users WHERE user_id = $1', [id])
-    .then(result => {
-      return res.status(204).send();
-    })
-    .catch(err => console.error(err.stack));
+  await UsersModel.deleteUser(id);
+
+  return res.status(204).send();
 };
 
 module.exports = {
--- 07_Backend/05_API-pg/models/UsersModel.js
+++ 07_Backend/05_API-pg/models/UsersModel.js
@@ -1,4 +1,14 @@
-const createUser = (email, password, db) => {
+const { Pool } = require('pg');
+
+const db = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'qwe123',
+  port: 5432
+});
+
+const createUser = (email, password) => {
   return db
     .query('INSERT INTO users (email, password) VALUES($1, $2) RETURNING *', [
       email,
@@ -8,4 +18,49 @@ const createUser = (email, password, db) => {
     .catch(err => console.error(err.stack));
 };
 
-module.exports = { createUser };
+const getAllUsers = () => {
+  return db
+    .query('SELECT * FROM users')
+    .then(result => result.rows)
+    .catch(err => console.error(err.stack));
+};
+
+const getUserById = id => {
+  return db
+    .query('SELECT * FROM users WHERE user_id = $1', [id])
+    .then(result => result.rows[0])
+    .catch(err => console.error(err.stack));
+};
+
+const updateUser = (email, password, id) => {
+  return db
+    .query(
+      'UPDATE users SET email = $1, password = $2 WHERE user_id = $3 RETURNING *',
+      [email, password, id]
+    )
+    .then(result => result.rows[0])
+    .catch(err => console.error(err.stack));
+};
+
+const updatePartialUser = (property, value, id) => {
+  return db
+    .query(`UPDATE users SET ${property} = '${value}' WHERE user_id = ${id}`)
+    .then(result => result.rows)
+    .catch(err => console.error(err.stack));
+};
+
+const deleteUser = id => {
+  return db
+    .query('DELETE FROM users WHERE user_id = $1', [id])
+    .then(result => result.rows)
+    .catch(err => console.error(err.stack));
+};
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  updatePartialUser,
+  deleteUser
+};
