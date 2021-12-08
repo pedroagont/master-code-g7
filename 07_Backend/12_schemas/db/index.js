e5ed0bc19ec8595fac51a2bae1b9cf77b9580ec8
Pedro A. González <pgonzalez@post.com>
2021-12-07 21:44:25 -0500

    Instalar dotenv y configurar llaves de entorno + gitignore .env

--- 07_Backend/12_schemas/db/index.js
+++ 07_Backend/12_schemas/db/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb+srv://pedroagont:qwe123@cluster0.ol6yb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    `mongodb+srv://${process.env.MONGODB_DATABASE_USERNAME}:${process.env.MONGODB_DATABASE_PASSWORD}@cluster0.ol6yb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
   )
   .then(() => console.log('Conectado a la base de datos'))
   .catch(err => console.log('Error conectando', err.message));
